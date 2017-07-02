9b7ca0bd6730e858363fbf2205858189453ba7a9
Jon Bardin <diclophis@gmail.com>
2017-07-02 02:07:45 -0700

    cleanup whitespace

--- lib/oktakit/client/authentication.rb
+++ lib/oktakit/client/authentication.rb
@@ -8,35 +8,35 @@ module Oktakit
       def authn(username, password, mfa)
         response, http_status = post('/authn', {'username' => username, 'password' => password})
 
-				if 200 == http_status 
-					if "SUCCESS" == response[:status]
-						return true
-					elsif "MFA_REQUIRED" == response[:status] 
-						if (state_token = response[:stateToken]) && (embedded = response[:_embedded])
-							if factors = embedded[:factors]
-								factors.each do |factor|
-									if "token:software:totp" == factor[:factorType]
-										if links = factor[:_links]
-											if verify = links[:verify]
+        if 200 == http_status
+          if "SUCCESS" == response[:status]
+            return true
+          elsif "MFA_REQUIRED" == response[:status]
+            if (state_token = response[:stateToken]) && (embedded = response[:_embedded])
+              if factors = embedded[:factors]
+                factors.each do |factor|
+                  if "token:software:totp" == factor[:factorType]
+                    if links = factor[:_links]
+                      if verify = links[:verify]
                         if href = verify[:href]
-													data = {
-														'fid': factor[:id],
-														'stateToken': state_token,
-														'passCode': mfa
-													}
+                          data = {
+                            'fid': factor[:id],
+                            'stateToken': state_token,
+                            'passCode': mfa
+                          }
 
-        									response, http_status = post(href.sub(api_endpoint, ''), data)
+                          response, http_status = post(href.sub(api_endpoint, ''), data)
 
                           return response, http_status
-												end
-											end
-										end
-									end
-								end
-							end
-						end
-					end
-				end
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
