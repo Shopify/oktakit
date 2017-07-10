fbf4a0328f5c7a1ebf323a42536078c9044325d1
Jon Bardin <diclophis@gmail.com>
2017-07-10 10:29:06 -0700

    cleanup warnings in base

--- lib/oktakit/error.rb
+++ lib/oktakit/error.rb
@@ -88,7 +88,7 @@ module Oktakit
     end
 
     def redact_url(url_string)
-      %w(client_secret access_token).each do |token|
+      %w[client_secret access_token].each do |token|
         url_string.gsub!(/#{token}=\S+/, "#{token}=(redacted)") if url_string.include? token
       end
       url_string
