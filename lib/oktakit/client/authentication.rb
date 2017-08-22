4f638cefbba4f61a7978070cc2b7cbc54e4bb5f7
Jon Bardin <diclophis@gmail.com>
2017-08-22 14:45:13 -0700

    fix authorization algorith to ensure MFA rules are enforced

--- lib/oktakit/client/authentication.rb
+++ lib/oktakit/client/authentication.rb
@@ -11,37 +11,44 @@ module Oktakit
       def authn(username, password, mfa)
         response, http_status = post('/authn', 'username' => username, 'password' => password)
 
-        factors = []
-
         if http_status == 200
           if response[:status] == "SUCCESS"
             return response, http_status
-          elsif response[:status] == "MFA_REQUIRED"
-            if (state_token = response[:stateToken]) && (embedded = response[:_embedded])
-              factors = embedded[:factors]
+          else
+            if response[:status] == "MFA_REQUIRED" || response[:status] == "MFA_CHALLENGE"
+              factors = []
+              if (state_token = response[:stateToken]) && (embedded = response[:_embedded])
+                if embedded[:factors]
+                  factors = embedded[:factors]
+                end
+              end
+
+              factors.each do |factor|
+                next unless factor[:factorType] == "token:software:totp"
+
+                links = factor[:_links]
+                verify = links[:verify]
+                href = verify[:href]
+
+                next unless links && verify && href
+
+                data = {
+                  'fid' => factor[:id],
+                  'stateToken' => state_token,
+                  'passCode' => mfa
+                }
+
+                response, http_status = post(href.sub(api_endpoint, ''), data)
+
+                if response[:status] == "SUCCESS" && http_status == 200
+                  return response, http_status
+                end
+              end
             end
           end
         end
 
-        factors.each do |factor|
-          next unless factor[:factorType] == "token:software:totp"
-
-          links = factor[:_links]
-          verify = links[:verify]
-          href = verify[:href]
-
-          next unless links && verify && href
-
-          data = {
-            'fid' => factor[:id],
-            'stateToken' => state_token,
-            'passCode' => mfa
-          }
-
-          response, http_status = post(href.sub(api_endpoint, ''), data)
-
-          return response, http_status
-        end
+        raise Oktakit::Unauthorized.new
       end
     end
   end
