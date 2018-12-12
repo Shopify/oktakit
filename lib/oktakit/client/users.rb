3ffb4efdc807df7f050fb4eacb7672fdd5edc59c
Pascal Charest <pascal.charest@gmail.com>
2018-12-12 14:05:21 -0500

    API support for Users/Clear_user_sessions (#13)
    * adding support for clear_user_sessions api endpoint

--- lib/oktakit/client/users.rb
+++ lib/oktakit/client/users.rb
@@ -9,7 +9,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns the created User.
-      # @see http://developer.okta.com/docs/api/resources/users.html#create-user
+      # @see https://developer.okta.com/docs/api/resources/users#create-user
       # @example
       #   Oktakit.create_user
       def create_user(options = {})
@@ -25,7 +25,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Fetched User
-      # @see http://developer.okta.com/docs/api/resources/users.html#get-user
+      # @see https://developer.okta.com/docs/api/resources/users#get-user
       # @example
       #   Oktakit.get_user('id')
       def get_user(id, options = {})
@@ -40,7 +40,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Array<Sawyer::Resource>] Array of User
-      # @see http://developer.okta.com/docs/api/resources/users.html#list-users
+      # @see https://developer.okta.com/docs/api/resources/users#list-users
       # @example
       #   Oktakit.list_users
       def list_users(options = {})
@@ -56,7 +56,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Updated User
-      # @see http://developer.okta.com/docs/api/resources/users.html#update-user
+      # @see https://developer.okta.com/docs/api/resources/users#update-user
       # @example
       #   Oktakit.update_user('id')
       def update_user(id, options = {})
@@ -72,7 +72,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Updated User
-      # @see http://developer.okta.com/docs/api/resources/users.html#update-profile
+      # @see https://developer.okta.com/docs/api/resources/users#update-profile
       # @example
       #   Oktakit.update_profile('id')
       def update_profile(id, options = {})
@@ -87,7 +87,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Array<Sawyer::Resource>] Array of App Links
-      # @see http://developer.okta.com/docs/api/resources/users.html#get-assigned-app-links
+      # @see https://developer.okta.com/docs/api/resources/users#get-assigned-app-links
       # @example
       #   Oktakit.get_assigned_app_links('id')
       def get_assigned_app_links(id, options = {})
@@ -103,7 +103,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Array<Sawyer::Resource>] Array of Groups
-      # @see http://developer.okta.com/docs/api/resources/users.html#get-member-groups
+      # @see https://developer.okta.com/docs/api/resources/users#get-member-groups
       # @example
       #   Oktakit.get_member_groups('id')
       def get_member_groups(id, options = {})
@@ -119,7 +119,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns empty object by default. If sendEmail is false, a user activation link.
-      # @see http://developer.okta.com/docs/api/resources/users.html#activate-user
+      # @see https://developer.okta.com/docs/api/resources/users#activate-user
       # @example
       #   Oktakit.activate_user('id')
       def activate_user(id, options = {})
@@ -135,7 +135,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object.
-      # @see http://developer.okta.com/docs/api/resources/users.html#deactivate-user
+      # @see https://developer.okta.com/docs/api/resources/users#deactivate-user
       # @example
       #   Oktakit.deactivate_user('id')
       def deactivate_user(id, options = {})
@@ -151,7 +151,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object.
-      # @see http://developer.okta.com/docs/api/resources/users.html#suspend-user
+      # @see https://developer.okta.com/docs/api/resources/users#suspend-user
       # @example
       #   Oktakit.suspend_user('id')
       def suspend_user(id, options = {})
@@ -167,7 +167,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object. Returns an empty object.
-      # @see http://developer.okta.com/docs/api/resources/users.html#unsuspend-user
+      # @see https://developer.okta.com/docs/api/resources/users#unsuspend-user
       # @example
       #   Oktakit.unsuspend_user('id')
       def unsuspend_user(id, options = {})
@@ -183,7 +183,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object
-      # @see http://developer.okta.com/docs/api/resources/users.html#unlock-user
+      # @see http://developer.okta.com/docs/api/resources/users#unlock-user
       # @example
       #   Oktakit.unlock_user('id')
       def unlock_user(id, options = {})
@@ -199,7 +199,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object by default. If sendEmail is false a password reset link
-      # @see http://developer.okta.com/docs/api/resources/users.html#reset-password
+      # @see https://developer.okta.com/docs/api/resources/users#reset-password
       # @example
       #   Oktakit.reset_password('id')
       def reset_password(id, options = {})
@@ -215,7 +215,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns the user by default. If tempPassword is true, a temporary password.
-      # @see http://developer.okta.com/docs/api/resources/users.html#expire-password
+      # @see https://developer.okta.com/docs/api/resources/users#expire-password
       # @example
       #   Oktakit.expire_password('id')
       def expire_password(id, options = {})
@@ -231,7 +231,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object by default.
-      # @see http://developer.okta.com/docs/api/resources/users.html#reset-factors
+      # @see https://developer.okta.com/docs/api/resources/users#reset-factors
       # @example
       #   Oktakit.reset_factors('id')
       def reset_factors(id, options = {})
@@ -247,7 +247,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Returns an empty object by default. If sendEmail is false a password reset link
-      # @see http://developer.okta.com/docs/api/resources/users.html#forgot-password
+      # @see https://developer.okta.com/docs/api/resources/users#forgot-password
       # @example
       #   Oktakit.forgot_password('id')
       def forgot_password(id, options = {})
@@ -263,7 +263,7 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Credentials of the user
-      # @see http://developer.okta.com/docs/api/resources/users.html#change-password
+      # @see https://developer.okta.com/docs/api/resources/users#change-password
       # @example
       #   Oktakit.change_password('id')
       def change_password(id, options = {})
@@ -279,11 +279,26 @@ module Oktakit
       # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
       # @param options [Hash] Optional. Body params for request.
       # @return [Hash<Sawyer::Resource>] Credentials of the user
-      # @see http://developer.okta.com/docs/api/resources/users.html#change-recovery-question
+      # @see https://developer.okta.com/docs/api/resources/users#change-recovery-question
       # @example
       #   Oktakit.change_recovery_question('id')
       def change_recovery_question(id, options = {})
         post("/users/#{id}/credentials/change_recovery_question", options)
+      end
+
+      # Clear user sessions
+      #
+      # @params id [string] User ID
+      # @param options[:query] [Hash] Optional. Query params for request
+      # @param options[:headers] [Hash] Optional. Header params for the request.
+      # @param options[:accept] [String] Optional. The content type to accept. Default application/json
+      # @param options[:content_type] [String] Optional. The content type for the request. Default application/json
+      # @param options [Hash] Optional. Body params for the request.
+      # @return 204 'No Content'
+      # @see https://developer.okta.com/docs/api/resources/users#user-sessions
+      #   Oktakit.clear_user_sessions('id')
+      def clear_user_sessions(id, options = {})
+        delete("/users/#{id}/sessions", options)
       end
     end
   end
